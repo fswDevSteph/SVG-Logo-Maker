1433ac44904288bd3348b40c70c81bc36f224146
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-01-28 12:23:00 -0400

    getting organized

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-// 4. write tests with jest
-// 5. write inquierer prompts based on req's
-
-let testT = new Triangle(); //assuming triangle class is already written with its properties and methods
+//This will be main application file to use inquirer to prompt users and generate the logo.
