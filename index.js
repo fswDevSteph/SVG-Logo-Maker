68fe129df1546101925d36427f1d80f3e9c2165f
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-02-10 13:08:04 -0400

    added the shapes classes and code for rendering them

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 //This will be main application file to use inquirer to prompt and handle users imput and generate the logo.
 //"Entry" point for application and orchastrates the main functionality.
 const inquirer = require('inquirer'); // Importing the inquirer module.
-const { createShape } = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 //!Prompt function goes here
 // Prompt for text logo
 async function promptForText() {
   try {
-    const userInput = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters for your logo:',
-        validate: function (input) {
-          // Check if input length is less than or equal to 3
-          if (input.length > 3) {
-            return 'Please enter up to three characters.'; // If not, ask again.
-          }
-          return true; // Accept
+    const userInput = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter up to three characters for your logo:',
+          validate: function (input) {
+            // Check if input length is less than or equal to 3
+            if (input.length > 3) {
+              return 'Please enter up to three characters.'; // If not, ask again.
+            }
+            return true; // Accept
+          },
         },
-      },
-      {
-        type: 'input',
-        name: 'color',
-        message: 'Enter text color (color keyword or hexadecimal number):',
-        validate: function (userColorInput) {
-          // Validate if input is a valid color keyword or hexadecimal number
-          // The start ^ and the end $ indicate the pattern to match
-          // ^# this part means the input MUST start with # character
-          // \b wraps word matches
-          // | the pipe means an exact match of the previous pattern or the next
-          // .test() method that searches for exact match between a regular expression and specified string
-          if (
-            !/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$|^(\b(black|white|red|green|blue|yellow|magenta|cyan)\b)$/.test(
-              userColorInput
-            )
-          ) {
-            return 'Please enter a valid color keyword or hexadecimal number.';
-          }
-          return true;
+        {
+          type: 'input',
+          name: 'color',
+          message: 'Enter text color (color keyword or hexadecimal number):',
+          validate: function (userColorInput) {
+            // Validate if input is a valid color keyword or hexadecimal number
+            // The start ^ and the end $ indicate the pattern to match
+            // ^# this part means the input MUST start with # character
+            // \b wraps word matches
+            // | the pipe means an exact match of the previous pattern or the next
+            // .test() method that searches for exact match between a regular expression and specified string
+            if (
+              !/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$|^(\b(black|white|red|green|blue|yellow|magenta|cyan)\b)$/.test(
+                userColorInput
+              )
+            ) {
+              return 'Please enter a valid color keyword or hexadecimal number.';
+            }
+            return true;
+          },
         },
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Select a shape:',
-        choices: ['circle', 'triangle', 'square'], // Choices for the user to select from
-      },
-    ]);
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'Select a shape:',
+          choices: ['circle', 'triangle', 'square'], // Choices for the user to select from
+        },
+      ])
+      .then((answers) => {});
   } catch (error) {
     console.error(error);
   }
